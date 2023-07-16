9af6764aa2dad3d527aa36cbc84093d56edf4b1a
Aftab Sama <79740895+Aftab700@users.noreply.github.com>
2023-07-16 13:18:07 +0530

    Add files via upload

---
+++ QH/check_archive.py
@@ -0,0 +1,27 @@
+import os
+import re
+
+def is_compressed_file(filename, target_folder):
+  with open(filename, 'rb') as f:
+    header = f.read(4)
+  if header.startswith(b'PK') :
+    os.rename(filename, os.path.join(target_folder, os.path.basename(filename+".zip")))
+  elif header.startswith(b'ustar'):
+    os.rename(filename, os.path.join(target_folder, os.path.basename(filename+".tar")))
+  elif header.startswith(b'Rar!\x1A\x07\x00'):
+    os.rename(filename, os.path.join(target_folder, os.path.basename(filename+".rar")))
+
+
+def main():
+  target_folder = 'archive'
+  if not os.path.exists(target_folder):
+    os.mkdir(target_folder)
+  for filename in os.listdir('.'):
+    if filename.endswith('.py') or filename == target_folder:
+      continue
+    is_compressed_file(filename, target_folder)
+
+if __name__ == '__main__':
+  main()
+
+print("Done...")
