bf84ecd865a364b0faaad191c0bf63b76d01d7f6
Aftab Sama <79740895+Aftab700@users.noreply.github.com>
2023-07-25 12:57:35 +0530

    Create move_pe_nonpe_files.py

---
+++ QH/move_pe_nonpe_files.py
@@ -0,0 +1,34 @@
+import openpyxl
+
+wb = openpyxl.load_workbook('scan_report.xls')
+sheet = wb['undetected']
+PE_values = []
+NON_PE_values = []
+
+for row in range(2, sheet.max_row + 1):
+    cell_value = sheet.cell(row=row, column=1).value
+    if cell_value != None:
+        PE_values.append(cell_value)
+
+for row in range(2, sheet.max_row + 1):
+    cell_value = sheet.cell(row=row, column=2).value
+    if cell_value != None:
+        NON_PE_values.append(cell_value)
+
+print(PE_values)
+print(NON_PE_values)
+
+for i in PE_values:
+    try:
+        os.rename(f"./sample/{i}", f"./PE/{i}")
+    except Exception as e:
+        print(e)
+
+for i in NON_PE_values:
+    try:
+        os.rename(f"./sample/{i}", f"./NON_PE/{i}")
+    except Exception as e:
+        print(e)
+
+
+print("\n\nDone...")
