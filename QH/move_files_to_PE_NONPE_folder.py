28be338e35c4cb6b7d7fa607f20d0dc13a2d05f5
Aftab Sama <79740895+Aftab700@users.noreply.github.com>
2023-08-08 20:27:07 +0530

    Add files via upload

---
+++ QH/move_files_to_PE_NONPE_folder.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+import hashlib
+import stat
+
+
+def move_files_to_PE_NONPE_directory():
+    if not os.path.exists("sample"):
+        os.mkdir("sample")
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            src = os.path.join(root, file)
+            dst = os.path.join('.sample/', file)
+            if src == dst or src.endswith(".txt") or src.endswith(".py"):
+                continue
+            if os.path.exists(dst):
+                base, ext = os.path.splitext(file)
+                i = 1
+                while os.path.exists(dst):
+                    dst = os.path.join('.', f'{base}_{i}{ext}')
+                    i += 1
+            shutil.move(src, dst)
+            print(src)
+
+
+if __name__ == "__main__":
+    move_files_to_PE_NONPE_directory()
+    pass
+
+
